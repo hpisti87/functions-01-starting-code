570e37408d5a2a94e1ca782567a79f0d8cd676af
hpisti87 <pisti87@gmail.com>
2022-08-15 18:36:11 +0200

    Section 6: 129

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 const startGameBtn = document.getElementById('start-game-btn');
+
+const start = function () {
+  console.log('Game is starting...');
+};
+
+console.dir(startGame);
+
+startGameBtn.addEventListener('click', start);
