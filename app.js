b1dda8610ea6c58e8716cef70f0225231947e453
hpisti87 <pisti87@gmail.com>
2022-08-27 10:23:56 +0200

    Section 6: 138

--- app.js
+++ app.js
@@ -67,3 +67,25 @@ startGameBtn.addEventListener('click', () => {
   alert(message);
   gameIsRunning = false;
 });
+
+// Rest parameter:
+
+const sumUp = (...numbers) => {
+  let sum = 0;  
+  for (const num of numbers) {
+    sum += num;
+  }
+  return sum;
+}
+
+const subtractUp = function() {
+  let sum = 0;  
+  for (const num of arguments) { // Don't use that!
+    sum -= num;
+  }
+  return sum;
+}
+
+console.log(sumUp(1, 5, 10, -3, 6, 10));
+console.log(sumUp(1, 5, 10, -3, 6, 10, 25, 88));
+console.log(subtractUp(1, 10, 15, 20));
